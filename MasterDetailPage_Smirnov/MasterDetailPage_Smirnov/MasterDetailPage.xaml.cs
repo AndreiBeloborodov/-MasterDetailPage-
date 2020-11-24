9f704a2bb0fc0db930ee8b8ca52a1b56d27435aa
opilane <opilane@HP-CZC6297B8H>
2020-11-24 12:30:15 +0200

    Add project files.

---
+++ MasterDetailPage_Smirnov/MasterDetailPage_Smirnov/MasterDetailPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetailPage_Smirnov
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MasterDetailPage() => InitializeComponent();
+
+        public bool IsPresented { get; private set; }
+
+        void OnButtonClick(object sender, EventArgs e)
+        {
+            // hide master page
+            this.IsPresented = false;
+            // show master page
+            this.IsPresented = true;
+        }
+    }
